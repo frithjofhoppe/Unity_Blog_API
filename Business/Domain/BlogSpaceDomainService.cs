84c729800f913171646218878adf73ee865ecc9c
frithjofhoppe <frithjof.hoppe@icloud.com>
2018-02-19 16:45:43 +0100

    create entities

--- Business/Domain/BlogSpaceDomainService.cs
+++ Business/Domain/BlogSpaceDomainService.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Business.Entity;
 
 namespace Business.Domain
 {
     public class BlogSpaceDomainService : IBlogSpaceDomainService
     {
+        public void CreateBlogSpaceIfNotExists(BlogSpace blogSpace)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Member> GetAllBlogSpaces()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Member> GetAllBlogSpacesByMemberId(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
